ce979ce2c336080d90c468d995c6359c04fe5a9a
sakamoto ryota <ryota.sakamoto@elvez.co.jp>
2018-04-06 14:29:55 +0900

    Add autsideClickNext option to index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -646,6 +646,12 @@ declare module 'driver.js' {
       allowClose?: boolean,
 
       /**
+       * Clicking outside the highlighted element should move next
+       * @default false
+       */
+      outsideClickNext?: boolean,
+
+      /**
        * Background color for the stage behind the highlighted element
        * @default '#ffffff'
        */
