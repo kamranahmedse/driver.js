d5be768403428ae567c1cfde1893c9d88f23ceda
Kamran Ahmed <kamranahmed.se@gmail.com>
2018-08-10 17:45:54 +0400

    Add popover positioning on right bottom

--- demo/scripts/demo.js
+++ demo/scripts/demo.js
@@ -8,21 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
         title: 'Before we start',
         description: 'This is just one use-case, make sure to check out the rest of the docs below.',
         nextBtnText: 'Okay, Start!',
-        position: 'right-center'
+        position: 'right-bottom'
       },
     }, {
       element: '#logo_img',
       popover: {
         title: 'Focus Anything',
         description: 'You can use it to highlight literally anything, images, text, div, span, li etc.',
-        position: 'right-center',
+        position: 'right-bottom',
       },
     }, {
       element: '#name_driver',
       popover: {
         title: 'Why Driver?',
         description: 'Because it lets you drive the user across the page',
-        position: 'right-center'
+        position: 'right-bottom'
       }
     }, {
       element: '#driver-demo-head',
