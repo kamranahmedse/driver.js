7d64866b89252912cd736b04ee3063853c174c0c
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-06-25 13:40:10 +0100

    Update design

--- docs/src/components/examples/ExampleButton.tsx
+++ docs/src/components/examples/ExampleButton.tsx
@@ -9,7 +9,7 @@ export function ExampleButton(props: ExampleButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="bg-transparent rounded-xl py-2 px-4 font-medium text-yellow-300 border-2 border-yellow-300 text-lg hover:bg-yellow-300 hover:text-black transition-colors duration-100">
+      className="bg-transparent rounded-xl py-2 px-4 font-medium text-black border-2 border-black text-lg hover:bg-yellow-300 hover:text-black transition-colors duration-100">
       { text }
     </button>
   );
---
+++ docs/src/pages/docs/installation.md
@@ -0,0 +1,7 @@
+---
+layout: '../../layouts/DocsLayout.astro'
+title: Installation
+description: Installation instructions for the React Native Firebase library.
+---
+
+# Installation
