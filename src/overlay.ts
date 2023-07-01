9dbe470f6a538dcb0e5186f5958df7f6f355a14d
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-07-01 15:42:32 +0100

    Add theming guide

--- src/overlay.ts
+++ src/overlay.ts
@@ -112,7 +112,7 @@ function createOverlaySvg(stage: StageDefinition): SVGSVGElement {
   const windowY = window.innerHeight;
 
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.classList.add("driver-stage", "driver-stage-animated");
+  svg.classList.add("driver-overlay", "driver-overlay-animated");
 
   svg.setAttribute("viewBox", `0 0 ${windowX} ${windowY}`);
   svg.setAttribute("xmlSpace", "preserve");
