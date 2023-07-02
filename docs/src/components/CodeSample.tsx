342d4d9ec5e49ef3278ecf66edf443b4c9adf6f9
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-07-02 02:08:56 +0100

    Add example for async tour

--- docs/src/components/CodeSample.tsx
+++ docs/src/components/CodeSample.tsx
@@ -1,6 +1,7 @@
 import type { Config, DriveStep } from "driver.js";
 import { driver } from "driver.js";
 import "driver.js/dist/driver.css";
+import { useState } from "react";
 
 type CodeSampleProps = {
   heading: string;
@@ -14,27 +15,81 @@ type CodeSampleProps = {
   children: any;
 };
 
+function removeDummyElement() {
+  const el = document.querySelector(".dynamic-el");
+  if (el) {
+    el.remove();
+  }
+}
+
+function mountDummyElement() {
+  const newDiv = (document.querySelector(".dynamic-el") || document.createElement("div")) as HTMLElement;
+
+  newDiv.innerHTML = "This is a new Element";
+  newDiv.style.display = "block";
+  newDiv.style.padding = "20px";
+  newDiv.style.backgroundColor = "black";
+  newDiv.style.color = "white";
+  newDiv.style.fontSize = "14px";
+  newDiv.style.position = "fixed";
+  newDiv.style.top = `${Math.random() * (500 - 30) + 30}px`;
+  newDiv.style.left = `${Math.random() * (500 - 30) + 30}px`;
+  newDiv.className = "dynamic-el";
+
+  document.body.appendChild(newDiv);
+}
+
 export function CodeSample(props: CodeSampleProps) {
+  const [driverObj, setDriverObj] = useState<any>(null);
   const { heading, id, children, className, config, highlight, tour } = props;
 
   function onClick() {
     if (highlight) {
       const driverObj = driver({
-        ...config
+        ...config,
       });
       driverObj.highlight(highlight);
+
+      setDriverObj(driverObj);
     } else if (tour) {
+      if (tour?.[2]?.popover?.title === "Next Step is Async") {
+        tour[2].popover.onNextClick = () => {
+          mountDummyElement();
+          driverObj.moveNext();
+        };
+
+        if (tour?.[3]?.element === ".dynamic-el") {
+          tour[3].onDeselected = () => {
+            removeDummyElement();
+          };
+
+          // @ts-ignore
+          tour[4].popover.onPrevClick = () => {
+            mountDummyElement();
+            driverObj.movePrevious();
+          }
+
+          // @ts-ignore
+          tour[3].popover.onPrevClick = () => {
+            removeDummyElement();
+            driverObj.movePrevious();
+          };
+        }
+      }
+
       const driverObj = driver({
         ...config,
         steps: tour,
       });
+
       driverObj.drive();
+      setDriverObj(driverObj);
     }
   }
 
   return (
     <div id={id} className={className}>
-      <p className="text-lg -mt-0 font-medium text-black -mb-3 rounded-md">{ heading }</p>
+      <p className="text-lg -mt-0 font-medium text-black -mb-3 rounded-md">{heading}</p>
       <div className="-mb-4">{children}</div>
       <button onClick={onClick} className="w-full rounded-md bg-black p-2 text-white">
         Show me an Example
--- docs/src/env.d.ts
+++ docs/src/env.d.ts
@@ -1,2 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  driverObj: any;
+}
