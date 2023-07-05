40f9ada79f75e23f9bc55e064267ee90dd0befe0
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-07-05 01:52:42 +0100

    Make sidebar responsive

--- docs/tailwind.config.cjs
+++ docs/tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      'sh': {
+        'raw': '(min-height: 750px)',
+      },
+      ...require('tailwindcss/defaultConfig').theme.screens,
+    },
     container: {
     },
     extend: {},
