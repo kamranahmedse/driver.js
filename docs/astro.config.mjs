4dcc03d3175eaaa3412e8e42d04b26ac332b4856
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-07-26 02:07:39 +0100

    Meta tags and build updates

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,17 +1,30 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   markdown: {
     shikiConfig: {
       // theme: "material-theme"
-      theme: "monokai"
+      theme: "monokai",
       // theme: 'poimandres'
-    }
+    },
   },
-  integrations: [tailwind(), react(), mdx()]
+
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+    compress({
+      css: false,
+      js: false,
+    }),
+  ],
 });
