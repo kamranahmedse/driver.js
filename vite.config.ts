f84b72b812b7310b108e6abd41ca117b2fba6b6a
protogenesis <32860504+protogenesis@users.noreply.github.com>
2023-09-04 18:26:00 +0800

    fix: type error in the vite configuration file. (#416)

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,7 @@ module.exports = defineConfig({
     rollupOptions: {
       output: {
         assetFileNames: assetInfo => {
-          return assetInfo.name === "style.css" ? `driver.css` : assetInfo.name;
+          return assetInfo.name === "style.css" ? `driver.css` : assetInfo.name as string;
         },
       },
     },
