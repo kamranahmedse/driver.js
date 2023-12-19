fe8ad69dcef1a56c42bc50b2d53b5ca07c706ad7
Tosin Oguntuyi <tcubed03@gmail.com>
2023-12-19 13:07:59 +0100

    Add new fix for hiding arrows from screen reades

--- src/popover.ts
+++ src/popover.ts
@@ -74,13 +74,23 @@ export function renderPopover(element: Element, step: DriveStep) {
     disableButtons,
     showProgress,
 
-    nextBtnText = getConfig("nextBtnText") || "Next &rarr;",
-    prevBtnText = getConfig("prevBtnText") || "&larr; Previous",
+    nextBtnText = getConfig("nextBtnText") || "Next",
+    prevBtnText = getConfig("prevBtnText") || "Previous",
     progressText = getConfig("progressText") || "{current} of {total}",
   } = step.popover || {};
 
+  const rightArrow = document.createElement("span");
+  rightArrow.setAttribute("aria-hidden", "true");
+  rightArrow.innerHTML = " &rarr;";
+
+  const leftArrow = document.createElement("span");
+  leftArrow.setAttribute("aria-hidden", "true");
+  leftArrow.innerHTML = "&larr; ";
+
   popover.nextButton.innerHTML = nextBtnText;
+  popover.nextButton.appendChild(rightArrow);
   popover.previousButton.innerHTML = prevBtnText;
+  popover.previousButton.insertBefore(leftArrow, popover.previousButton.firstChild);
   popover.progress.innerHTML = progressText;
 
   if (title) {
